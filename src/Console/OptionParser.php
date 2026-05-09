78822028487a0df208fe034b425fbc8eced9fbef
Sérgio Dinis Lopes <sergiodlopes@gmail.com>
2026-05-09 18:58:03 +0100

    Fix OptionParser to ensure short option validation only occurs for valid input lengths

--- src/Console/OptionParser.php
+++ src/Console/OptionParser.php
@@ -878,7 +878,7 @@ class OptionParser {
         if (substr($name, 0, 2) === '--') {
             return isset($this->_options[substr($name, 2)]);
         }
-        if ($name[0] === '-' && $name[1] !== '-') {
+        if (strlen($name) >= 2 && $name[0] === '-' && $name[1] !== '-') {
             return isset($this->_shortOptions[$name[1]]);
         }
 
